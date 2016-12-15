d6564f2226979ece52828378e6d5ad30d8582711
Derek Burdick <derek.burdick@genesys.com>
2016-12-14 23:07:14 -0500

    Hack implementation of NewRelic Application and Transaction interfaces.

---
+++ norelic.go
@@ -0,0 +1,52 @@
+package norelic
+
+import (
+	"net/http"
+
+	newrelic "github.com/newrelic/go-agent"
+)
+
+type application struct{}
+
+type transaction struct {
+	http.ResponseWriter
+	R *http.Request
+}
+
+func Newapplication() newrelic.Application {
+	return &application{}
+}
+
+func (nra application) StartTransaction(name string, w http.ResponseWriter, r *http.Request) newrelic.Transaction {
+	return &transaction{
+		w,
+		r}
+}
+
+func (nra application) RecordCustomEvent(eventType string, params map[string]interface{}) error {
+	return nil
+}
+
+func (nt transaction) End() error {
+	return nil
+}
+
+func (nt transaction) Ignore() error {
+	return nil
+}
+
+func (nt transaction) SetName(name string) error {
+	return nil
+}
+
+func (nt transaction) NoticeError(err error) error {
+	return nil
+}
+
+func (nt transaction) AddAttribute(key string, value interface{}) error {
+	return nil
+}
+
+func (nt transaction) StartSegmentNow() newrelic.SegmentStartTime {
+	return newrelic.SegmentStartTime{}
+}
